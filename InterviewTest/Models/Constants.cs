ff8c6894e17b83ac98f3cc6d2a0f13c9d9bb74d5
Saurabh Jain <s.jain@openenergymarket.com>
2016-02-22 09:17:16 +0530

    Implement a Settings class and data type to persist the Newsletter Format
    Also implement a static constants class which contains the required constant
    values and default init values to support the Settings Model.

---
+++ InterviewTest/Models/Constants.cs
@@ -0,0 +1,22 @@
+﻿using InterviewTest.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace InterviewTest.Models
+{
+    public static class Constants
+    {
+        public const char TripIdentifier = 'T';
+        public const char HostIdentifier = 'H';
+
+        public struct SettingKeys
+        {
+            public const string NewsLetterFormatKey = "NewsLetterFormat";
+        }
+
+        public static Dictionary<string, string> DefaultInitValues => 
+            new Dictionary<string, string> { { SettingKeys.NewsLetterFormatKey, "HHTT" } };
+    }
+}
---
+++ InterviewTest/Models/Settings.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using InterviewTest.Database;
+
+namespace InterviewTest.Models
+{
+    public class Settings : IPersistable
+    {
+        public Settings()
+        {}
+
+        public Settings(string key) : base()
+        {
+            Key = key;
+        }
+
+        public string Id { get; set; }
+        public string Key { get; set; }
+        public string Value { get; set; }        
+    }
+
+
+
+}
